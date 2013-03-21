274116308690bf182fcb856b094d2a9ac6a8f864
Timothy Crosley <timothy.crosley@gmail.com>
2013-03-21 00:35:09 -0400

    0.5.2: Fix how delete's are done to support appengine

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='DynamicForm',
-      version='0.5.1',
+      version='0.5.2',
       description='DynamicForm is an AJAX abstraction library for python - that enables different sections of a page to operate and be interacted with independently.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='http://www.dynamicform.org/',
-      download_url='https://github.com/timothycrosley/DynamicForm/blob/master/dist/DynamicForm-0.5.1.tar.gz?raw=true',
+      download_url='https://github.com/timothycrosley/DynamicForm/blob/master/dist/DynamicForm-0.5.2.tar.gz?raw=true',
       license = "GNU GPLv2",
-      install_requires=['webelements>=1.0.0-alpha.19',],
+      install_requires=['webelements>=1.0.0-alpha.22',],
       packages=['DynamicForm',],)
